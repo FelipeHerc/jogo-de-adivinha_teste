517e3eb1e862757f6be1f71d4d19eaca11cfa5be
Felipe Hercules De Almeida Oliveira <1001750@PPC01INFLL00319.pucminas.net>
2019-02-25 20:28:33 -0300

    #2: Modularizacao do metodo

--- adivinha.rb
+++ adivinha.rb
@@ -1,12 +1,4 @@
-segredo = 151
-tentativas = 1
-
-puts("Digite seu nome: ")
-
-nome = gets
-
-while (tentativas <= 3)
-
+def check?(segredo, nome, tentativas)
 	puts("\ndigite o valor, "+ nome +": ")
 
 	chute = gets
@@ -16,15 +8,33 @@ while (tentativas <= 3)
 	if segredo == chute.to_i
 	  tentativas = 3
 	  puts("Você acertou!")
-	
+	  return true if segredo == chute.to_i
 	elsif segredo > chute.to_i
 	  puts("o valor inserido é menor.")
+	  return false if segredo != chute.to_i 
 	elsif segredo < chute.to_i
 	  puts("o valor inserido é maior.")
+ 	  segredo != chute.to_i
 	else
   	puts("você errou!")
+	return false if	segredo != chute.to_i
 	end
+end
 
+segredo = 151
+tentativas = 1
+
+puts("Digite seu nome: ")
+
+nome = gets
+
+while (tentativas <= 3)
+
+	break if check?(segredo, nome, tentativas)
+	
 	tentativas += 1
 end
+
+
+
 
